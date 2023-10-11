9c7d2720ac88bf6062e01b31512b03dc14c0c317
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-11 11:55:21 -0600

    Project started

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { JournalApp } from './JournalApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JournalApp />
+  </React.StrictMode>,
+)
