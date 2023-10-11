5be4e0e179801adae76dc418b41a386817250e98
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-11 17:17:08 -0600

    Add. Design for Navbar and JournalLayout was created

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import { Box } from "@mui/material"
+import { NavBar } from "../components/NavBar";
+
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+
+            <NavBar drawerWidth={drawerWidth} />
+
+            {/* Sidebar drawerWidth */}
+
+
+            <Box
+                component='main'
+                sx={{ flexGrow: 1, p: 3 }}
+            >
+                {/* Toolbar */}
+
+                {children}
+            </Box>
+
+
+        </Box>
+    );
+};
+
+
+JournalLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,18 @@
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 
 export const JournalPage = () => {
     return (
         <>
-            <Typography variant='h1'>JournalPage</Typography>
+            <JournalLayout>
+
+                <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ducimus amet est labore numquam! Asperiores atque veritatis tenetur, natus dolor, obcaecati amet fugiat rem voluptatibus nulla tempore dolorum dolores at!</Typography>
+
+                {/* Nothing selected */}
+                {/* NoteView */}
+
+            </JournalLayout>
         </>
     )
 }
