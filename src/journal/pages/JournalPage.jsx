f9ac1e3816e1fe75345a854e0017922e82830546
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-11 18:43:06 -0600

    Add. Floating Button created.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 
@@ -7,8 +9,22 @@ export const JournalPage = () => {
         <>
             <JournalLayout>
 
-                {/* <NothingSelectedView /> */}
-                <NoteView />
+                <NothingSelectedView />
+                {/* <NoteView /> */}
+
+                <IconButton
+                    size='large'
+                    sx={{
+                        color: 'white',
+                        backgroundColor: 'error.main',
+                        ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                        position: 'fixed',
+                        right: 50,
+                        bottom: 50,
+                    }}
+                >
+                    <AddOutlined sx={{ fontSize: 30 }} />
+                </IconButton>
 
             </JournalLayout>
         </>
