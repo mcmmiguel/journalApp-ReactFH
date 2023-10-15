6d233b9b7a29e4f5e8758520cdec7a190c9e96ba
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-15 13:59:43 -0600

    Feat. Create a new note on firebase

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,20 @@
+import { useDispatch } from 'react-redux'
 import { IconButton } from '@mui/material'
 import { AddOutlined } from '@mui/icons-material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
+import { startNewNote } from '../../store/journal'
 
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch();
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote());
+    }
+
+
     return (
         <>
             <JournalLayout>
@@ -13,6 +23,7 @@ export const JournalPage = () => {
                 {/* <NoteView /> */}
 
                 <IconButton
+                    onClick={onClickNewNote}
                     size='large'
                     sx={{
                         color: 'white',
