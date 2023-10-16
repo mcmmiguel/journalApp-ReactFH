f59cdeab849d4857286ba08955fdcad9b8f38843
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-16 14:18:54 -0600

    Test. Delete image from cloudinary

--- __tests__/helpers/fileUpload.test.js
+++ __tests__/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary'
 import { fileUpload } from "../../src/helpers/fileUpload";
+
+cloudinary.config({
+    cloud_name: 'dmrhtcc5z',
+    api_key: '823788935155713',
+    api_secret: 'Hab8dsrdPLC1V2zsqTLptEFvdq4',
+    secure: true,
+});
 
 
 describe('Pruebas en fileUpload', () => {
@@ -15,6 +23,24 @@ describe('Pruebas en fileUpload', () => {
         const url = await fileUpload(file);
 
         expect(typeof url).toBe('string');
+
+        // console.log(url);
+        const segments = url.split('/');
+        const imageId = segments[segments.length - 1].replace('.jpg', '');
+        const cloudinaryResp = await cloudinary.api.delete_resources(['journal/' + imageId], {
+            resource_type: 'image'
+        });
+        console.log({ cloudinaryResp });
+
+    });
+
+    test('Debe de retornar Null', async () => {
+
+        const file = new File([], 'prueba-imagen.jpg');
+
+        const url = await fileUpload(file);
+
+        expect(url).toBe(null);
 
     });
 
