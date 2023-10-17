f957f41aca69b2ff760b83a72119eaacde7349a8
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-16 21:34:56 -0600

    Test. StartNewNote and deleting notes from firebase

--- __tests__/store/journal/thunks.test.js
+++ __tests__/store/journal/thunks.test.js
@@ -1,4 +1,7 @@
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "../../../src/store/journal/journalSlice";
 import { startNewNote } from "../../../src/store/journal/thunks";
+import { firebaseDB } from "../../../src/firebase/config";
 
 
 describe('Pruebas en journal thunks', () => {
@@ -15,6 +18,32 @@ describe('Pruebas en journal thunks', () => {
         getState.mockReturnValue({ auth: { uid: uid } })
 
         await startNewNote()(dispatch, getState);
+
+        expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+        expect(dispatch).toHaveBeenCalledWith(addNewEmptyNote({
+            body: '',
+            title: '',
+            id: expect.any(String),
+            date: expect.any(Number),
+            imageURLs: expect.any(Array),
+        }));
+        expect(dispatch).toHaveBeenCalledWith(setActiveNote({
+            body: '',
+            title: '',
+            id: expect.any(String),
+            date: expect.any(Number),
+            imageURLs: expect.any(Array),
+        }));
+
+        // Borrar de Firebase
+        const collectionRef = collection(firebaseDB, `${uid}/journal/notes`);
+        const docs = await getDocs(collectionRef);
+
+        const deletePromises = [];
+        docs.forEach(doc => deletePromises.push(deleteDoc(doc.ref)));
+
+        await Promise.all(deletePromises);
+
 
     });
 
