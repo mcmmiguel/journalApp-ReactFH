adb6e7b42552ee02100114aad7cb958518bf4bb9
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-16 18:28:02 -0600

    Test. Success and Error with signInWithGoogle

--- __tests__/store/auth/thunks.test.js
+++ __tests__/store/auth/thunks.test.js
@@ -1,5 +1,7 @@
-import { checkingCredentials } from "../../../src/store/auth/authSlice";
-import { checkingAuthentication } from "../../../src/store/auth/thunks";
+import { signInWithGoogle } from "../../../src/firebase/providers";
+import { checkingCredentials, login, logout } from "../../../src/store/auth/authSlice";
+import { checkingAuthentication, startGoogleSignIn } from "../../../src/store/auth/thunks";
+import { testUser } from "../../fixtures/authFixtures";
 
 jest.mock('../../../src/firebase/providers');
 
@@ -13,6 +15,34 @@ describe('Pruebas en auth thunks', () => {
 
         await checkingAuthentication()(dispatch);
         expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+
+    });
+
+    test('startGoogleSignIn debe llamar checkingCredentials y login - Exito', async () => {
+
+        const loginData = { ok: true, ...testUser, };
+        await signInWithGoogle.mockResolvedValue(loginData);
+
+        // thunk
+        await startGoogleSignIn()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(login(loginData));
+
+    });
+
+    test('startGoogleSignIn debe llamar checkingCredentials y logout - Mensaje de Error', async () => {
+
+        const errorMessage = 'Error de Google'
+
+        const loginData = { ok: false, errorMessage };
+        await signInWithGoogle.mockResolvedValue(loginData);
+
+        // thunk
+        await startGoogleSignIn()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage));
 
     });
 
