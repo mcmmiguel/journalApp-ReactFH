a620fa8f7df390899cb8e30d7eb97c029793405b
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-05 20:27:11 -0600

    Production environment

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,17 +5,17 @@ import { getEnvironments } from "../helpers/getEnvironments";
 
 
 // console.log(import.meta.env);
-const {
-    VITE_APIKEY,
-    VITE_AUTHDOMAIN,
-    VITE_PROJECTID,
-    VITE_STORAGEBUCKET,
-    VITE_MESSAGINGSENDERID,
-    VITE_APPID,
-} = getEnvironments();
+// const {
+//     VITE_APIKEY,
+//     VITE_AUTHDOMAIN,
+//     VITE_PROJECTID,
+//     VITE_STORAGEBUCKET,
+//     VITE_MESSAGINGSENDERID,
+//     VITE_APPID,
+// } = getEnvironments();
 
 // Dev / Prod
-/*const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCVOxyXhn39eNzJ2Xy7hGa4WSbmcN0eDBw",
     authDomain: "journalapp-reactfh.firebaseapp.com",
     projectId: "journalapp-reactfh",
@@ -23,17 +23,17 @@ const {
     messagingSenderId: "1049888725386",
     appId: "1:1049888725386:web:ea72276d1b11b13ffe9073"
 };
-*/
+
 
 // Testing 
-const firebaseConfig = {
-    apiKey: VITE_APIKEY,
-    authDomain: VITE_AUTHDOMAIN,
-    projectId: VITE_PROJECTID,
-    storageBucket: VITE_STORAGEBUCKET,
-    messagingSenderId: VITE_MESSAGINGSENDERID,
-    appId: VITE_APPID,
-};
+// const firebaseConfig = {
+//     apiKey: VITE_APIKEY,
+//     authDomain: VITE_AUTHDOMAIN,
+//     projectId: VITE_PROJECTID,
+//     storageBucket: VITE_STORAGEBUCKET,
+//     messagingSenderId: VITE_MESSAGINGSENDERID,
+//     appId: VITE_APPID,
+// };
 
 export const firebaseApp = initializeApp(firebaseConfig);
 
